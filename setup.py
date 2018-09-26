334ed5dd93050ff6b1c5ef919d5820fb50697c5a
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-09-25 22:47:00 -0500

    Initial commit - 2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     author="Joao Gilberto Magalhaes",
     author_email="joao@byjg.com.br",
     description="Common Terraobject package",
-    long_description="Common Terraobject package",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://bitbucket.org/byjg/terraobject",
     packages=setuptools.find_packages(),
